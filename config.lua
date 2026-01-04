5c4d0bc77ff5d945b0e2e9dbe127a597000f9126
Loafabreadly <birdgeekthree@gmail.com>
2026-01-04 06:40:10 -0700

    feat: Add initial configuration settings for the application

---
+++ config.lua
@@ -0,0 +1,19 @@
+Config = {}
+
+-- Debug mode for development
+Config.Debug = false
+
+-- Target system to use: 'ox_target', 'qb-target', 'qtarget'
+-- Will auto-detect if not specified
+Config.TargetSystem = nil
+
+-- Notification system to use: 'rpa-notify', 'ox_lib', 'qb-core'
+-- Will auto-detect if not specified
+Config.NotifySystem = nil
+
+-- TextUI system to use: 'rpa-textui', 'ox_lib', 'qb-core'
+-- Will auto-detect if not specified
+Config.TextUISystem = nil
+
+-- Default notification duration (ms)
+Config.NotifyDuration = 5000
